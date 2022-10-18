b90f091c99e045cc1260c70011ff31c102cd8c98
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-10-18 14:06:31 +0200

    migrate deploy from vercel

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -2,46 +2,62 @@ import { dataSet } from "./data/data.mjs";
 import { data } from "./data/dataCoord.mjs";
 import { PrismaClient } from "@prisma/client";
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
-// let dataAgency = [];
-// async function findAndCreate() {
-//   // dataAgency = dataSet.agencies.map((agency) => {
-//   for (let agency of dataSet.agencies) {
-//     const cityId = await prisma.city.findUnique({
-//       where: {
-//         city: agency.city,
-//       },
+let dataAgency = [];
+async function findAndCreate() {
+  // dataAgency = dataSet.agencies.map((agency) => {
+  for (let agency of dataSet.agencies) {
+    const cityId = await prisma.city.findUnique({
+      where: {
+        city: agency.city,
+      },
+    });
+    console.log(agency, cityId);
+    dataAgency.push({
+      name: agency.name,
+      city: agency.city,
+      region: agency.region,
+      size: agency.companySize,
+      website: agency.website,
+      logo: agency.eguideImageSrc,
+      cityId: cityId.id,
+    });
+    console.log(agency.name, agency.city, agency.id);
+  }
+
+  await prisma.city.deleteMany();
+
+  // for (let city of data.coord) {
+  await prisma.city.createMany({
+    data: data,
+  });
+  // }
+
+  // for (let agency of dataAgency) {
+  await prisma.agency.createMany({
+    data: dataAgency,
+  });
+  // }
+}
+
+findAndCreate()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => {
+    prisma.$disconnect();
+  });
+
+// async function main() {
+//   for (let city of data.coord) {
+//     await prisma.city.create({
+//       data: city,
 //     });
-//     console.log(agency, cityId);
-//     dataAgency.push({
-//       name: agency.name,
-//       city: agency.city,
-//       region: agency.region,
-//       size: agency.companySize,
-//       website: agency.website,
-//       logo: agency.eguideImageSrc,
-//       cityId: cityId.id,
-//     });
-//     console.log(agency.name, agency.city, agency.id);
 //   }
-
-//   await prisma.city.deleteMany();
-
-//   // for (let city of data.coord) {
-//   await prisma.city.createMany({
-//     data: data,
-//   });
-//   // }
-
-//   // for (let agency of dataAgency) {
-//   await prisma.agency.createMany({
-//     data: dataAgency,
-//   });
-//   // }
 // }
-
-// findAndCreate()
+// main()
 //   .catch((e) => {
 //     console.log(e);
 //     process.exit(1);
@@ -49,19 +65,3 @@ import { PrismaClient } from "@prisma/client";
 //   .finally(() => {
 //     prisma.$disconnect();
 //   });
-
-// // async function main() {
-// //   for (let city of data.coord) {
-// //     await prisma.city.create({
-// //       data: city,
-// //     });
-// //   }
-// // }
-// // main()
-// //   .catch((e) => {
-// //     console.log(e);
-// //     process.exit(1);
-// //   })
-// //   .finally(() => {
-// //     prisma.$disconnect();
-// //   });
