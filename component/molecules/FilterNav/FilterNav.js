cfdd93556b9d1310bce8c34922b24e6d1b68632d
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-10-28 11:22:09 +0200

    stop delete

--- component/molecules/FilterNav/FilterNav.js
+++ component/molecules/FilterNav/FilterNav.js
@@ -21,16 +21,16 @@ const FilterNav = ({ children, variant, ...props }) => {
   const modalId = "filterOptions";
   const toggleMap = useMapProvider().toggleMap;
   const mapIsOpen = useMapProvider().mapIsOpen;
-  const resetPagination = usePagination().reset;
+  // const resetPagination = usePagination().reset;
 
   function sendFilterInput(e) {
     if (e.target.id == "modal") {
       filter.submitFilterInput();
-      reset();
+      // reset();
       modal.toggleModal();
     } else {
       filter.submitFilterInput();
-      reset();
+      // reset();
     }
   }
 
