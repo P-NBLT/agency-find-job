78076238e19082a3541cebd7717d52ba52ad3901
Pierre-Alexis Noblet <98712114+P-NBLT@users.noreply.github.com>
2022-10-28 11:14:20 +0200

    change db3

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -10,6 +10,9 @@ const prisma = new PrismaClient();
 
 let dataAgency = [];
 async function findAndCreate() {
+  await prisma.city.createMany({
+    data: data,
+  });
   // dataAgency = dataSet.agencies.map((agency) => {
   for (let agency of dataSet.agencies) {
     const cityId = await prisma.city.findUnique({
@@ -30,12 +33,10 @@ async function findAndCreate() {
     console.log(agency.name, agency.city, agency.id);
   }
 
-  await prisma.city.deleteMany();
+  // await prisma.city.deleteMany();
 
   // for (let city of data.coord) {
-  await prisma.city.createMany({
-    data: data,
-  });
+
   // }
 
   // for (let agency of dataAgency) {
@@ -54,65 +55,65 @@ findAndCreate()
     prisma.$disconnect();
   });
 
-async function main() {
-  for (let city of data) {
-    await prisma.city.create({
-      data: city,
-    });
-  }
+// async function main() {
+//   for (let city of data) {
+//     await prisma.city.create({
+//       data: city,
+//     });
+//   }
+// }
+// main()
+// .catch((e) => {
+//   console.log(e);
+//   process.exit(1);
+// })
+// .finally(() => {
+//   prisma.$disconnect();
+
+async function createUserAdmin() {
+  const password = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
+  const userAdmin = await prisma.user.create({
+    data: {
+      email: "pierrealexis.noblet@gmail.com",
+      password: password,
+      firstName: "Pierre-Alexis",
+      lastName: "Noblet",
+      admin: true,
+      verified: true,
+    },
+  });
 }
-main()
+
+createUserAdmin()
   .catch((e) => {
     console.log(e);
     process.exit(1);
   })
   .finally(() => {
     prisma.$disconnect();
-
-    async function createUserAdmin() {
-      const password = await bcrypt.hash(process.env.ADMIN_PASSWORD, 10);
-      const userAdmin = await prisma.user.create({
-        data: {
-          email: "pierrealexis.noblet@gmail.com",
-          password: password,
-          firstName: "Pierre-Alexis",
-          lastName: "Noblet",
-          admin: true,
-          verified: true,
-        },
-      });
-    }
-
-    createUserAdmin()
-      .catch((e) => {
-        console.log(e);
-        process.exit(1);
-      })
-      .finally(() => {
-        prisma.$disconnect();
-      });
-
-    // async function dashboardSimulation() {
-    //   const sample = await prisma.dashboard.create({
-    //     data: {
-    //       userName: "Paul",
-    //       emailUser: "Paul@gmail.com",
-    //       companyName: "BigTech",
-    //       city: "Rotterdam",
-    //       region: "Zuid-Holland",
-    //       size: "GT-100",
-    //       website: "http://www.humanoids.nl/",
-    //       logo: "https://eguide.nl/media/output/100_100/1_humanoids_logo.png",
-    //       payload: "create",
-    //     },
-    //   });
-    // }
-
-    // dashboardSimulation()
-    //   .catch((e) => {
-    //     console.log(e);
-    //     process.exit(1);
-    //   })
-    //   .finally(() => {
-    //     prisma.$disconnect();
   });
+
+// async function dashboardSimulation() {
+//   const sample = await prisma.dashboard.create({
+//     data: {
+//       userName: "Paul",
+//       emailUser: "Paul@gmail.com",
+//       companyName: "BigTech",
+//       city: "Rotterdam",
+//       region: "Zuid-Holland",
+//       size: "GT-100",
+//       website: "http://www.humanoids.nl/",
+//       logo: "https://eguide.nl/media/output/100_100/1_humanoids_logo.png",
+//       payload: "create",
+//     },
+//   });
+// }
+
+// dashboardSimulation()
+//   .catch((e) => {
+//     console.log(e);
+//     process.exit(1);
+//   })
+//   .finally(() => {
+//     prisma.$disconnect();
+// });
